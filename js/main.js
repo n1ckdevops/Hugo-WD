b5de59e7a5a8822632e7dbe57998b85d03fce7ac
n1ckdevops <nickdevops6@gmail.com>
2024-03-17 23:38:55 +0300

    add: modal and finish project

--- js/main.js
+++ js/main.js
@@ -67,3 +67,22 @@ document.addEventListener("input", (e) => {
     input.value = result;
   }
 });
+
+// MODAL
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModalBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
